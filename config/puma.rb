a0b387a8965b593b06a01e0b5fdf58923dc5652d
mechdog-home <mark@kenney.io>
2026-01-27 13:42:17 -0500

    Change default port to 6767

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests.
+# Default changed to 6767 to avoid conflicts with other Rails apps.
+port ENV.fetch("PORT", 6767)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
